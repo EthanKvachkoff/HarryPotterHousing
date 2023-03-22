30cd31db1cc0fcc8293f1bdb4ce0d7c262a05d04
Ethan Kvachkoff <112204336+EthanKvachkoff@users.noreply.github.com>
2023-03-22 18:09:39 -0500

    Added some windows
    Added two buttons and two windows. When pressing the information button, will pop up a window that will soon display all users house information.

--- src/application/QuizController.java
+++ src/application/QuizController.java
@@ -3,9 +3,33 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
 public class QuizController implements Initializable{
+    
+    @FXML
+    private Button userInfoButton;
+    
+    @FXML
+    void handleInfoButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("InfoBox.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("House Information");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module HarryPotterHousing {
 	requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
