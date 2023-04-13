c80d4fda5b694ab2ecca5a84647b236e8d9cc52f
Zach Butler <zbutl@live.com>
2023-04-13 16:18:53 -0400

    Added user ID and file writing stuff
    Added user ID variables and all of the methods necessary for writing to and reading from files.

--- src/application/User.java
+++ src/application/User.java
@@ -2,23 +2,34 @@
 // Author: Zach Butler
 package application;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class User {
 
     // Instance variables
+	int id;
     String name, pet;
+    
     
     //Aggregation relationship
     private House house;
     
     //Non-default constructor
-    public User(String name, String pet, House house) {
+    public User(int id, String name, String pet, House house) {
         super();
+        this.id = id;
         this.name = name;
         this.pet = pet;
         this.house = house;
     }
     
     // Get methods
+    public int getId() {
+    	return id;
+    }
+    
     public String getName() {
         return name;
     }
@@ -32,6 +43,10 @@ public class User {
     }
     
     // Set methods
+    public void setId(int id) {
+    	this.id = id;
+    }
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -42,6 +57,51 @@ public class User {
     
     public void setHouse(House house) {
         this.house = house;
+    }
+    
+ // Reads user's id, name, pet, and house from given file
+    public void read(String filename) {
+        File f = new File(filename);
+        try (Scanner scan = new Scanner(f)) {
+        	id = scan.nextInt();
+            name = scan.nextLine();
+            pet = scan.nextLine();
+            house.name = scan.nextLine();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Reads multiple objects from the same file
+    public void read(Scanner scan) {
+        if (scan.hasNext()) {
+        	id = scan.nextInt();
+            name = scan.nextLine();
+            pet = scan.nextLine();
+            house.name = scan.nextLine();
+        }
+    }
+
+    // Write user's id, name, pet, and house to file
+    public void write(String filename) {
+
+        try (FileWriter f = new FileWriter(filename)) {
+            f.write(id + "\n" + name + "\n" + pet + "\n" + house.name + "\n");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Write to file with FileWriter object
+    public void write(FileWriter f) {
+        try {
+            f.write(id + "\n" + name + "\n" + pet + "\n" + house.name + "\n");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
