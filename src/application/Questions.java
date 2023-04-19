1301361c4698e410a08ff06a7e7f437bac2e505f
Ethan Kvachkoff <112204336+EthanKvachkoff@users.noreply.github.com>
2023-04-19 17:06:34 -0500

    Fixed issues
    Fixed issues

--- src/application/Questions.java
+++ src/application/Questions.java
@@ -45,5 +45,15 @@ public class Questions {
     public void setAnswers(String answerss[]) {
         this.answers = answerss;
     }
-    
+    // Used to return the largest number of points in an array
+    // Used to check for which house the user suits best with
+    public static int getLargestNumber(int[] arr) {
+        int highestNum = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[highestNum]) {
+                highestNum = i;  
+            }
+        }
+        return highestNum;
+    }
 }
